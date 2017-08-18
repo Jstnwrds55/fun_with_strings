fbbe69543fe706216616aac24bf38af92d8572ee
Justin <jstnwrds55@gmail.com>
2017-08-18 12:45:47 -0600

    Now accepts user input to translate. Now outputs as sentence instead of list.

--- pigLatin.py
+++ pigLatin.py
@@ -1,14 +1,18 @@
+print('What sentence would you like to translate to Pig-Latin?')
+inputSentence = input()
+
+
 def pigLatin(userSentence):
     wordList = userSentence.split()
     for x in range(0, len(wordList)):
         if wordList[x] == 'a':
-            wordList[x] = 'away'
+            wordList[x] = 'a-way'
         elif wordList[x][0] in ['a', 'e', 'i', 'o', 'u']:
             wordList[x] += '-way'
-            print(wordList)
         else:
             wordList[x] = wordList[x][1:len(wordList[x])] + '-' + wordList[x][0] + 'ay'
-            print(wordList)
-    print(wordList)
-    return wordList
+    translatedSentence = ' '.join(wordList)
+    print(translatedSentence)
+    return translatedSentence
 
+pigLatin(inputSentence)
