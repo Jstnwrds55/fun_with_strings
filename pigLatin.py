26af77d1cece2772c5eb735face02c472f2c1a20
Justin <jstnwrds55@gmail.com>
2017-08-18 15:45:13 -0600

    Added capitals to list of beginning vowels

--- pigLatin.py
+++ pigLatin.py
@@ -13,7 +13,7 @@ def pigLatin(userSentence):
             pass
         if len(wordList[x]) == 1:
             wordList[x] += '-way'
-        elif wordList[x][0] in ['a', 'e', 'i', 'o', 'u']:
+        elif wordList[x][0] in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
             if lastLetter.isalpha():
                 wordList[x] += '-way'
             else:
