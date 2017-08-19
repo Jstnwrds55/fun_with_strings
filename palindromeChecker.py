9b06ff447c103cd2499e545b7ead2f4fe4b269d9
Justin <jstnwrds55@gmail.com>
2017-08-19 15:29:39 -0600

    Palindrome checker now strips capitals, spaces, and punctuation to work with sentences and more complex sayings. Also accepts user input to check palindrome.

--- palindromeChecker.py
+++ palindromeChecker.py
@@ -1,7 +1,14 @@
+print('What word do you need to check for palindromeism? (I don\'t think that\'s a real word).')
+userText = input()
+
+
 def checkIfPalindrome(text):
-    listText = list(text)
-    reverseText = list(reversed(text))
+    textStripped = (''.join([i for i in text if i.isalpha()]).lower())
+    listText = list(textStripped)
+    reverseText = list(reversed(textStripped))
     if listText == reverseText:
         print(True)
     else:
         print(False)
+
+checkIfPalindrome(userText)
